03ac9775f7f50223926a3e0af4fbeccad0556240
Benjamin Benni <benjamin.benni06@gmail.com>
2015-06-26 15:57:17 +0200

    Initial travis test
    Push fake project to test travis CI (pushed under Sebastien's pressure)

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ecoknowledgeApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the ecoknowledgeApp
+ */
+angular.module('ecoknowledgeApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
