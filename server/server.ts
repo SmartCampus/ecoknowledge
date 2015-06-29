d030e2548ffdcab46617b822a49bca270bbec6a1
Benjamin Benni <benjamin.benni06@gmail.com>
2015-06-29 16:46:28 +0200

    Add routes into server
    Add addbadge, get goals/badges and evaluatebadge into server's routes

--- server/server.ts
+++ server/server.ts
@@ -11,6 +11,8 @@ var xmlParser = bodyParser.text({type: "application/xml"});
 
 import Goal = require('./Goal');
 import User = require('./User');
+import Badge = require('./Badge');
+
 var currentUser:User = new User("Jackie");
 
 // Enable JSON data for requests
@@ -41,6 +43,33 @@ app.get('/helloworld', jsonParser, function (req, res, next) {
   res.send("Hello  world!");
 });
 
+app.get('/goals', jsonParser, function (req, res, next) {
+  console.log('getgoals');
+  var goals = currentUser.getGoals();
+
+  var result = [];
+  for(var i in goals) {
+    var currentGoalDesc = goals[i];
+    result.push(currentGoalDesc);
+  }
+console.log("Sending", result);
+  res.send(result);
+});
+
+app.get('/badges', jsonParser, function (req, res, next) {
+  console.log('getbadges');
+  var badges = currentUser.getBadges();
+
+  var result = [];
+  for(var i in badges) {
+    var currentBadgeDesc = badges[i];
+    result.push(currentBadgeDesc);
+  }
+  console.log("Sending", result);
+
+  res.send(result);
+});
+
 
 app.post('/addgoal', jsonParser, function (req, res) {
   var actionData = req.body;
@@ -56,6 +85,39 @@ app.post('/addgoal', jsonParser, function (req, res) {
 
   res.send(result);
 });
+
+app.post('/addbadge', jsonParser, function (req, res) {
+  var actionData = req.body;
+  console.log(actionData);
+
+  var badgeName:string = actionData.name;
+  var badgeDescription:string = actionData.description;
+  var badgeObjective:string = actionData.objective;
+  var badgePoints:number = actionData.points;
+
+  var goal = currentUser.retrieveGoal(badgeObjective);
+  console.log("Log pour le badge en cours .. ", goal);
+
+  var badge:Badge = new Badge(badgeName, badgeDescription, badgePoints, goal);
+
+  currentUser.addBadge(badge);
+
+
+  res.send("OK");
+});
+
+
+app.post('/evaluatebadge',jsonParser, function(req, res) {
+  var actionData = req.body;
+  console.log(actionData);
+
+  var badgeName:string = actionData.name;
+  var badgeValue:number = actionData.value;
+  var result = currentUser.evaluateBadge(badgeName, badgeValue);
+  res.send(result);
+
+});
+
 
 //  For debug only
 app.post('/evaluategoal',jsonParser, function(req, res) {
