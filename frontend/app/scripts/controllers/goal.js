3e907f3e8abe46270b3df4f7a3355885ddf47941
Tom Dall'Agnol <dallagnol.tom@etu.unice.fr>
2015-06-30 17:55:53 +0200

    Add possibility to add more than one comparator for a goal

--- frontend/app/scripts/controllers/goal.js
+++ frontend/app/scripts/controllers/goal.js
@@ -3,17 +3,30 @@
 (function () {
   angular.module('ecoknowledgeApp')
     .controller('GoalCtrl', ['ServiceGoal', function (ServiceGoal) {
-      this.goal = {};
 
       var self = this;
+      self.goal = {};
+      self.goal.conditions = [];
+      self.goal.name = '';
+
+
+      self.type = ['Température', 'Porte'];
 
       this.addGoal = function () {
         console.log(self.goal);
         ServiceGoal.post(self.goal,function() {
-            console.log('Achieve to add a goal', self.goal);
+            console.log('Achieve to add a conditions', self.conditions);
           },function(){
-            console.log('Fail when trying to add a goal', self.goal);
+            console.log('Fail when trying to add a conditions', self.conditions);
         });
       };
+
+      self.addComparison = function(){
+          this.obj = {};
+          this.obj.required=self.type[0];
+          self.goal.conditions[self.goal.conditions.length] = this.obj;
+      };
+
+      self.addComparison();
     }]);
 })();
