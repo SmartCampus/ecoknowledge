1397808913d45d825b86b2d4f42ea5fc80e76ce2
Tom Dall'Agnol <dallagnol.tom@etu.unice.fr>
2015-07-01 15:57:37 +0200

    Test of ServiceGoal and ServiceBadge
    Test in frontend with mocks Issue #7, #1, #8

---
+++ frontend/test/spec/controllers/ServiceBadgeTest.js
@@ -0,0 +1,77 @@
+'use strict';
+
+var path = 'http://localhost:3000/';
+
+describe('Service: ServiceBadge', function() {
+    var Badge, httpBackend;
+
+    //load the service's module
+    beforeEach(module('ecoknowledgeApp'));
+
+    //Initialize the controller and a mock Scope
+    beforeEach(inject(function (_$httpBackend_, _ServiceBadge_) {
+        httpBackend = _$httpBackend_;
+        Badge = _ServiceBadge_;
+    }));
+
+    describe('Badge.get', function () {
+        var callbacks;
+
+        beforeEach(function () {
+            callbacks = {
+                success: function () {
+             },
+                error: function () {
+                }
+            };
+            spyOn(callbacks, 'success').and.callThrough();
+            spyOn(callbacks, 'error').and.callThrough();
+        });
+
+
+        it('should get all the badges from the service', function () {
+            var mockMonBadge = { goal: 'Mon goal', points: '37', description: 'Une description', name: 'MonBadge' };
+            var mockMonBadgePatrick = { goal: 'Mon goal de fou', points: '42', description: 'Un de scription', name: 'MonBadgePatrick' };
+            var mockResult = [mockMonBadge, mockMonBadgePatrick];
+            //backend definition returns a mock user
+            httpBackend.when('GET',path +'badges/').respond(mockResult);
+            httpBackend.expectGET(path+'badges/');
+            Badge.get('', callbacks.success, callbacks.error);
+
+            httpBackend.flush();
+
+            expect(callbacks.success).toHaveBeenCalled();
+            expect(callbacks.error).not.toHaveBeenCalled();
+            expect(callbacks.success.calls.argsFor(0)).toEqual([mockResult]);
+        });
+    });
+
+    describe('Badge.post', function () {
+        var callbacks;
+
+        beforeEach(function () {
+            callbacks = {
+                success: function () {
+                },
+                error: function () {
+                }
+            };
+            spyOn(callbacks, 'success').and.callThrough();
+            spyOn(callbacks, 'error').and.callThrough();
+        });
+
+        it('should send a new goal to the service', function(){
+            var mockMonBadgePatrick = { goal: 'Mon goal de fou', points: '42', description: 'Un de scription', name: 'MonBadgePatrick' };
+
+            httpBackend.when('POST',path +'addbadge');
+            httpBackend.expectPOST(path+'addbadge').respond('gg wp');
+            Badge.post(mockMonBadgePatrick, callbacks.success, callbacks.error);
+
+            httpBackend.flush();
+
+            expect(callbacks.success).toHaveBeenCalled();
+            expect(callbacks.error).not.toHaveBeenCalled();
+        });
+
+    });
+});
--- frontend/test/spec/controllers/ServiceGoalTest.js
+++ frontend/test/spec/controllers/ServiceGoalTest.js
@@ -1,50 +1,54 @@
 'use strict';
 
-describe('Service: ServiceGoal', function(){
-   var Goal, httpBackend;
+var path = 'http://localhost:3000/';
+
+describe('Service: ServiceGoal', function() {
+    var Goal, httpBackend;
 
     //load the service's module
     beforeEach(module('ecoknowledgeApp'));
 
     //Initialize the controller and a mock Scope
-    beforeEach(inject(function(_$httpBackend_, _ServiceGoal_){
+    beforeEach(inject(function (_$httpBackend_, _ServiceGoal_) {
         httpBackend = _$httpBackend_;
         Goal = _ServiceGoal_;
     }));
 
-    describe('Goal.get', function(){
-       var callbacks;
+    describe('Goal.get', function () {
+        var callbacks;
 
-        beforeEach(function(){
-           callbacks = {
-               success : function(data){},
-               error : function(data){}
-           };
+        beforeEach(function () {
+            callbacks = {
+                success: function () {
+                },
+                error: function () {
+                }
+            };
             spyOn(callbacks, 'success').and.callThrough();
             spyOn(callbacks, 'error').and.callThrough();
         });
 
 
-        it('should get a goal from the service', function(){
-            var mockClim = {"name": "Clim","conditions" :
-                [
-                    {"required":"Temperature", "comparison" : "inf", "value": 25},
-                    {"required":"Temperature", "comparison" : "eq", "value": 18},
-                    {"required":"Temperature", "comparison" : "inf", "value": 25}
+        it('should get some goals from the service', function () {
+            var mockClim = {
+                'name': 'Clim', 'conditions': [
+                    {'required': 'Temperature', 'comparison': 'inf', 'value': 25},
+                    {'required': 'Temperature', 'comparison': 'eq', 'value': 18},
+                    {'required': 'Temperature', 'comparison': 'inf', 'value': 25}
                 ]
             };
-            var mockPatrick = { "name": "Patrick","conditions" :
-                [
-                    {"required":"Porte", "comparison" : "inf", "value": "OPEN"},
-                    {"required":"Temperature", "comparison" : "eq", "value": 18},
-                    {"required":"Temperature", "comparison" : "inf", "value": 25}
+            var mockPatrick = {
+                'name': 'Patrick', 'conditions': [
+                    {'required': 'Porte', 'comparison': 'inf', 'value': 'OPEN'},
+                    {'required': 'Temperature', 'comparison': 'eq', 'value': 18},
+                    {'required': 'Temperature', 'comparison': 'inf', 'value': 25}
                 ]
             };
             var mockResult = [mockClim, mockPatrick];
             //backend definition returns a mock user
-            httpBackend.when('GET', 'http://localhost:3000/goals/').respond(mockResult);
-            httpBackend.expectGET('http://localhost:3000/goals/');
-            Goal.get('',callbacks.success,callbacks.error);
+            httpBackend.when('GET',path +'goals/').respond(mockResult);
+            httpBackend.expectGET(path+'goals/');
+            Goal.get('', callbacks.success, callbacks.error);
 
             httpBackend.flush();
 
@@ -52,5 +56,41 @@ describe('Service: ServiceGoal', function(){
             expect(callbacks.error).not.toHaveBeenCalled();
             expect(callbacks.success.calls.argsFor(0)).toEqual([mockResult]);
         });
+    });
+
+    describe('Goal.post', function () {
+        var callbacks;
+
+        beforeEach(function () {
+            callbacks = {
+                success: function () {
+                },
+                error: function () {
+                }
+            };
+            spyOn(callbacks, 'success').and.callThrough();
+            spyOn(callbacks, 'error').and.callThrough();
+        });
+
+        it('should send a new goal to the service', function(){
+            var mockPatrick = {
+                'name': 'Patrick', 'conditions': [
+                    {'required': 'Porte', 'comparison': 'inf', 'value': 'OPEN'},
+                    {'required': 'Temperature', 'comparison': 'eq', 'value': 18},
+                    {'required': 'Temperature', 'comparison': 'inf', 'value': 25}
+                ]
+            };
+
+
+            httpBackend.when('POST',path +'adddgoal');
+            httpBackend.expectPOST(path+'addgoal').respond('gg wp');
+            Goal.post(mockPatrick, callbacks.success, callbacks.error);
+
+            httpBackend.flush();
+
+            expect(callbacks.success).toHaveBeenCalled();
+            expect(callbacks.error).not.toHaveBeenCalled();
+        });
+
     });
 });
